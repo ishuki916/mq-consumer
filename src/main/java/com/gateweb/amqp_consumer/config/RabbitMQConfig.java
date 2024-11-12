adec327a63f8c90bb8eeafa6a5c39885195b96e9
Maki <makiliu@gateweb.com.tw>
2024-11-12 18:22:28 +0800

    add MessageConverter

---
+++ src/main/java/com/gateweb/amqp_consumer/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package com.gateweb.amqp_consumer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
--- src/main/java/com/gateweb/amqp_consumer/usecase/AmqpConsumerService.java
+++ src/main/java/com/gateweb/amqp_consumer/usecase/AmqpConsumerService.java
@@ -1,4 +1,24 @@
 package com.gateweb.amqp_consumer.usecase;
 
+import com.gateweb.amqp_consumer.domain.CustomMsg;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class AmqpConsumerService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(queues = "myQueue")
+    public void receiveMessage(@Payload CustomMsg message, @Header("myHeaderKey") String myHeaderKey) {
+        log.debug("Received message: {}", message);
+        log.debug("Received myHeaderKey: {}", myHeaderKey);
+    }
 }
